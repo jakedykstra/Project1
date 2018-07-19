c88463d748d1bb13ec87e394af33f440689a462c
Jake Dykstra <jacob.d.dykstra@gmail.com>
2018-07-18 21:06:29 -0700

    very entry

---
+++ assets/javascript/app.js
@@ -0,0 +1,16 @@
+// Trade history database
+
+// tdb if i need these params within (dependant on where the info needed are stored)
+function tradeHistory(transaction, coinAmount, usdAmount) {
+    // need to have database for all the firebase transactions
+
+}
+
+// Splash page unless session storage
+function checkUser() {
+    // check for session storage
+    if(localStorage.getItem()){
+        newUser === false;
+        // if modal - $('.modal').style('display', 'name')
+        // if changing to different path - location.replace('./index.html')  
+    }
