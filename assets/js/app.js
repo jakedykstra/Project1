e97d5dfb0cc4bd459f72cace8638e97faeeb51f2
los4673 <35182425+los4673@users.noreply.github.com>
2018-07-27 15:34:14 -0700

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,11 +20,8 @@ $(document).ready(function () {
     jQuery.getJSON("https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETH,XRP,LTC&tsyms=USD", function (data) {
       bitcoinPrice = data.RAW.BTC.USD.PRICE;
       ethereumPrice = data.RAW.ETH.USD.PRICE;
-      ripplePrice = data.RAW.LTC.USD.PRICE;
-      litecoinPrice = data.RAW.XRP.PRICE;
-      console.log(data)
-      console.log(bitcoinPrice);
-      console.log(ethereumPrice);
+      ripplePrice = data.RAW.XRP.USD.PRICE;
+      litecoinPrice = data.RAW.LTC.USD.PRICE;
       reAvaluate();
     });
     //1. Estimate total purchase amount
@@ -33,7 +30,8 @@ $(document).ready(function () {
     //4. Credit Crypto amount 
     //5. Calls Revaulate function
   
-    function buyCrypto(amount, crypto) {
+    function buyCrypto(amount, crypto, objCrypto) {
+      console.log("the function read this as: " + crypto);
       if (!amount || typeof amount === 'Number') {
         console.log('Invalid input');
         return;
@@ -46,46 +44,51 @@ $(document).ready(function () {
       } else {
         console.log("you have enough");
         person.USD -= amount;
-        person.ETH += totalAmount;
+        person[objCrypto] += totalAmount;
       }
-      console.log("total amount USD: " + amount);
-      console.log("total amount ETH: " + totalAmount);
-      console.log("Updated person USD: " + person.USD);
-      console.log("updated person ETH: " + person.ETH);
+      // console.log("total amount USD: " + amount);
+      // console.log("total amount ETH: " + totalAmount);
+      // console.log("Updated person USD: " + person.USD);
+      // console.log("updated person ETH: " + person.ETH);
       reAvaluate();
     }
   
   
   
     //Figures out current Balance of sell crypto
-    function sellCrypto(amount, crypto) {
+    function sellCrypto(amount, crypto, objCrypto) {
       console.log("sell button clicked");
       if (!amount || typeof amount === 'Number') {
         console.log('Invalid input');
         return;
       };
       var totalAmount = amount * crypto;
-      console.log("total amount of USD being sold in crypto" + totalAmount);
-      if (amount > person.ETH) {
+      // console.log("total amount of USD being sold in crypto" + totalAmount);
+      if (amount > person[objCrypto]) {
         console.log('you dont have enough');
       } else {
         console.log("you have enough");
         person.USD += totalAmount;
-        person.ETH -= amount;
-        console.log("total amount USD: " + totalAmount);
-        console.log("total amount ETH: " + amount);
-        console.log("Updated person USD: " + person.USD);
-        console.log("updated person ETH: " + person.ETH);
+        person[objCrypto] -= amount;
+        // console.log("total amount USD: " + totalAmount);
+        // console.log("total amount ETH: " + amount);
+        // console.log("Updated person USD: " + person.USD);
+        // console.log("updated person ETH: " + person.ETH);
       };
       reAvaluate();
     }
   
     //Reavulates crypto holdings to USD
     function reAvaluate() {
-      console.log(person.ETHVal);
+      // console.log(person.ETHVal);
+
+      //Updates Value on persons coins
       person.BTCVal = person.BTC * bitcoinPrice;
       person.ETHVal = person.ETH * ethereumPrice;
+      person.XRPVal = person.XRP * ripplePrice;
+      person.LTCVal = person.LTC * litecoinPrice;
       person.totalNet = person.BTCVal + person.ETHVal + person.USD;
+
       $("#jqueryUSD").text(person.USD.toLocaleString('en'));
       $("#jqueryNet").text(person.totalNet.toLocaleString('en'));
       $("#personBTC").text(person.BTC.toLocaleString('en'));
@@ -97,23 +100,36 @@ $(document).ready(function () {
       // $("#personRipple").text(person.XRP.toLocaleString('en'));
       // $("#XRPVal").text(person.XRPVal.toLocaleString('en'));
 
-      console.log("Updated Portfolio");
-      console.log("Ethereum value" + person.ETHVal);
+      // console.log("Updated Portfolio");
+      // console.log("Ethereum value" + person.ETHVal);
+
+      //Sets All the current prices for trading
+      $("#jqueryBTC").text(Math.floor(bitcoinPrice).toLocaleString('en'));
+      $("#jqueryLTC").text(litecoinPrice.toLocaleString('en'));
+      $("#jqueryEther").text(ethereumPrice.toLocaleString('en'));
+      $("#jqueryXRP").text(ripplePrice.toLocaleString('en'));
     }
   
-    $("#buyEther").on('click', function () {
+    $("#buybtc").on('click', function () {
       event.preventDefault();
-      var amount = parseInt($("#amount-eth").val().trim());
-      buyCrypto(amount, ethereumPrice);
-      $("#amount-eth").val("");
+      console.log("clicked");
+      var amount = parseInt($("#amount-btc").val().trim());
+      buyCrypto(amount, bitcoinPrice, "BTC");
+      $("#amount-btc").val("");
     });
   
-    $("#sellEther").on('click', function () {
+    $("#sellbtc").on('click', function () {
       event.preventDefault();
-      var amount = parseInt($('#amount-eth2').val().trim());
-      sellCrypto(amount, ethereumPrice);
-      $('#amount-eth2').val("");
-  
+      var amount = parseInt($('#amount-btc2').val().trim());
+      sellCrypto(amount, bitcoinPrice, "BTC");
+      $('#amount-btc2').val("");
+    });
+
+    $("#buyLTC").on('click', function () {
+      event.preventDefault();
+      var amount = parseInt($('#amount-ltc').val().trim());
+      buyCrypto(amount, litecoinPrice, "LTC");
+      $('#amount-ltc').val("");
     });
   
   });
